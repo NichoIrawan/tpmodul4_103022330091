60d969e604be58bd834872bf5f26bf9809052d1e
Nicholas Irawan <nicholas2815@hotmail.com>
2025-03-08 18:16:26 +0700

    menambahkan implementasi dengan table-driven

---
+++ KodePos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_103022330091
+{
+    class KodePos
+    {
+        public string kelurahan;
+        private String kodePos;
+
+        public KodePos(string kelurahan, string kodePos)
+        {
+            this.kelurahan = kelurahan;
+            this.kodePos = kodePos;
+        }
+
+        public string getKodePos()
+        {
+            return kodePos;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,31 @@
 ﻿
+using tpmodul4_103022330091;
+
+KodePos[] listKodePos = new KodePos[11];
+
+listKodePos[0] = new KodePos("Batununggal", "40266");
+listKodePos[1] = new KodePos("Kujangsari", "40287");
+listKodePos[2] = new KodePos("Mengger", "40267");
+listKodePos[3] = new KodePos("Wates", "40256");
+listKodePos[4] = new KodePos("Cijaura", "40287");
+listKodePos[5] = new KodePos("Jatisari", "40286");
+listKodePos[6] = new KodePos("Margasari", "40286");
+listKodePos[7] = new KodePos("Sekejati", "40286");
+listKodePos[8] = new KodePos("Kebonwaru", "40272");
+listKodePos[9] = new KodePos("Maleer", "40274");
+listKodePos[10] = new KodePos("Samoja", "40273");
+
+Console.WriteLine("Kelurahan\t| Kode Pos");
+Console.WriteLine("----------------|----------");
+
+foreach (KodePos kodePos in listKodePos)
+{
+    if (kodePos.kelurahan.Length > 7)
+    {
+        Console.WriteLine(kodePos.kelurahan + "\t| " + kodePos.getKodePos());
+    }
+    else
+    {
+        Console.WriteLine(kodePos.kelurahan + "\t\t| " + kodePos.getKodePos());
+    }
+}
